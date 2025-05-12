3ebd54affc7e5511d441e85e37db0cf9af6fd3b8
Wool5443 <mihsolodilov2015@gmail.com>
2025-05-13 00:24:40 +0300

    Fix string test
    Signed-off-by: Wool5443 <mihsolodilov2015@gmail.com>

--- tests/String.c
+++ tests/String.c
@@ -2,10 +2,18 @@
 
 int main()
 {
+    ERROR_CHECKING();
+
+    logger_init_console();
+
     String s = string_ctor("hi\n").value;
 
     String s2 = string_printf("Hello %d worlds!\n", 10).value;
 
     str_print(str_ctor_string(s), stdout);
     str_print(str_ctor_string(s2), stdout);
+
+ERROR_CASE
+    string_dtor(&s);
+    string_dtor(&s2);
 }
